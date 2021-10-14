0e4eac8031ca72b8444c9ffe7fc453f9ffae023d
NaimePasLesPseudos <naimdelben@gmail.com>
2021-10-14 10:55:52 +0200

    fix stroke

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,7 +13,7 @@
         </div>
         <button class="btn btn-square btn-ghost">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">             
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
             </svg>
         </button>
     </div>
@@ -64,9 +64,10 @@
                     </a> 
                 </li> 
             </ul>
+        </div>
     </div>
 </div>
-</div>
+
 </template>
 
 <script>
