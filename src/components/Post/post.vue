8ee63bda94bb67d4aa78e8152660d53d30d5cd9d
NaimePasLesPseudos <naimdelben@gmail.com>
2021-10-14 16:56:05 +0200

    first version for post and home page

--- src/components/Post/post.vue
+++ src/components/Post/post.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-
+<div class="post border-2 bg-base-100 p-5 mb-10">
+    <p class="text-xs mb-2">Publié par USER il y a DATE</p>
+    <h2 class="text-xl mb-2"> Un titre pour un magnifique article</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe officiis in, perferendis libero quidem assumenda dolore molestias consequuntur amet repellat tempora, odio quis facilis enim voluptates deleniti, temporibus quo eius?
+    Alias architecto, molestias, laudantium ullam eos odit quo labore quidem, cupiditate expedita inventore reiciendis. Impedit beatae officiis, a iusto voluptate maxime excepturi odit fugit fuga dignissimos tempore molestiae sit alias!
+    Minima explicabo cupiditate adipisci sequi quod veritatis voluptas cumque consequuntur. Est facere a, similique porro accusamus aliquid quia consectetur eveniet rem sunt error cupiditate debitis obcaecati quo accusantium exercitationem quas!</p>
+<div class="flex mt-2">
+      <p>💬 : 2 </p>
+      <p>👍 : 3</p>
+      <p>💡 : 3</p>
+      <p>🤣 : 3</p>
+      <p>💕 : 3</p>
     </div>
+</div>
 </template>
 
 <script setup>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,40 @@
 <template>
 <Header />
-  <div class="home">
-    <h1>This is a home page</h1>
-  </div>
+
+<div class="flex-col mx-10">
+
+<div class="post border-2 bg-base-100 p-5 mb-10">
+    <p class="text-xs mb-2">Publié par USER il y a DATE</p>
+    <h2 class="text-xl mb-2"> Un titre pour un magnifique article</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe officiis in, perferendis libero quidem assumenda dolore molestias consequuntur amet repellat tempora, odio quis facilis enim voluptates deleniti, temporibus quo eius?
+    Alias architecto, molestias, laudantium ullam eos odit quo labore quidem, cupiditate expedita inventore reiciendis. Impedit beatae officiis, a iusto voluptate maxime excepturi odit fugit fuga dignissimos tempore molestiae sit alias!
+    Minima explicabo cupiditate adipisci sequi quod veritatis voluptas cumque consequuntur. Est facere a, similique porro accusamus aliquid quia consectetur eveniet rem sunt error cupiditate debitis obcaecati quo accusantium exercitationem quas!</p>
+<div class="flex mt-2">
+      <p>💬 : 2 </p>
+      <p>👍 : 3</p>
+      <p>💡 : 3</p>
+      <p>🤣 : 3</p>
+      <p>💕 : 3</p>
+    </div>
+</div>
+
+<div class="post border-2 bg-base-100 p-5 mb-10">
+    <p class="text-xs mb-2">Publié par USER il y a DATE</p>
+    <h2 class="text-xl mb-2"> Un titre pour un magnifique article</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe officiis in, perferendis libero quidem assumenda dolore molestias consequuntur amet repellat tempora, odio quis facilis enim voluptates deleniti, temporibus quo eius?
+    Alias architecto, molestias, laudantium ullam eos odit quo labore quidem, cupiditate expedita inventore reiciendis. Impedit beatae officiis, a iusto voluptate maxime excepturi odit fugit fuga dignissimos tempore molestiae sit alias!
+    Minima explicabo cupiditate adipisci sequi quod veritatis voluptas cumque consequuntur. Est facere a, similique porro accusamus aliquid quia consectetur eveniet rem sunt error cupiditate debitis obcaecati quo accusantium exercitationem quas!</p>
+    <div class="flex mt-2">
+      <p>💬 : 2 </p>
+      <p>👍 : 3</p>
+      <p>💡 : 3</p>
+      <p>🤣 : 3</p>
+      <p>💕 : 3</p>
+    </div>
+</div>
+
+</div>
+
 <Footer />
 </template>
 
