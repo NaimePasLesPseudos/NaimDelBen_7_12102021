fa8c65dde97ff08a816dc8b118f5ce39bbb7cb09
NaimePasLesPseudos <naimdelben@gmail.com>
2021-10-15 09:43:47 +0200

    NavBar in french

--- src/components/header.vue
+++ src/components/header.vue
@@ -6,10 +6,10 @@
     <div class="flex-1 ">
         <img class="object-cover" alt="Groupomania logo" src="../assets/logo/icon-left-font-monochrome-white.png" />
         <a class="btn btn-link">
-            <router-link to="/">Home</router-link>
+            <router-link to="/">Accueil</router-link>
         </a> 
         <div class="form-control w-2/5">
-            <input type="text" placeholder="Search" class="input input-ghost">
+            <input type="text" placeholder="Recherche" class="input input-ghost">
         </div>
         <button class="btn btn-square btn-ghost">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">             
@@ -20,10 +20,10 @@
 
     <div class="">
         <a class="btn btn-link btn-secondary">
-            <router-link to="/signup">Signup</router-link>
+            <router-link to="/signup">Inscription</router-link>
         </a> 
         <a class="btn btn-link">
-            <router-link to="/login">Login</router-link>
+            <router-link to="/login">Connexion</router-link>
         </a>
         <div class="avatar">
             <div class="rounded-full w-10 h-10">
@@ -37,30 +37,30 @@
             <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
                 <li >
                     <a class="btn btn-ghost text-neutral">
-                        <router-link to="/profil">Profil</router-link>
+                        <router-link to="/user/1">Mon profil</router-link>
                     </a> 
                 </li> 
                 <li >
                     <a class="btn btn-link">
-                        <router-link to="/settings">Settings</router-link>
+                        <router-link to="/settings">Paramètres</router-link>
                     </a> 
                 </li> 
                 <li >
                     <a class="btn btn-link">
-                        <router-link to="/likes">My Likes</router-link>
+                        <router-link to="/likes">Mes favoris</router-link>
                     </a> 
                 </li> 
                     <div class="divider divide-primary "></div> 
                 <li >
                     <label class="cursor-pointer label">
-                        <span class="label-text">Dark Mode</span> 
+                        <span class="label-text">Mode Sombre</span> 
                         <input type="checkbox" checked="checked" class="toggle toggle-secondary">
                     </label>
                 </li> 
                     <div class="divider"></div> 
                 <li >
                     <a class="btn btn-link">
-                        <router-link to="/logout">Logout</router-link>
+                        <router-link to="/logout">Déconnexion</router-link>
                     </a> 
                 </li> 
             </ul>
