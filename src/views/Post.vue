d97e30c3abb38e7926a8befbb02cf13c1a404c16
NaimePasLesPseudos <naimdelben@gmail.com>
2021-10-15 19:13:13 +0200

    make the posts dynamic

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,18 +1,63 @@
 <template>
 <Header />
-  <div class="post">
-    <h1>This is a post page</h1>
-      <div>Item: {{ $route.params.id }}</div>
 
-  </div>
+
+<div class="flex-col mx-10">
+<h1 class="text-lg mb-5 ml-2"> En ce moment</h1>
+  <Post 
+      :title="post.title"
+      :userName="post.userName"
+      :content="post.content"
+      :comments="post.comments"
+      :thumbs="post.thumbs"
+      :lightBulbs="post.lightbulbs"
+      :rofls="post.rofls"
+      :hearts="post.hearts"
+      :id="post.id"
+  />
+
+</div>
+
+
 <Footer />
 </template>
 
 <script setup>
 import Header from '../components/header.vue'
 import Footer from '../components/footer.vue'
+import Post from '../components/Post/post.vue'
+import GroupomaniaData from "../services/Data"
+</script>
+
+<script>
+export default {
+  name: "post",
+  components: {
+    Post
+  },
+  data() {
+    return {
+      post: []
+    }
+  },
+  methods: {
+    searchPost() {
+      GroupomaniaData.getPost(this.$route.params.id)
+      .then((result) => {
+        this.post = result.data
+        console.log(result.data)
+      }).catch((err) => {
+        console.log(err)
+      });
+    }
+  },
+  mounted() {
+    this.searchPost()
+  }
+}
 </script>
 
 <style>
 
 </style>
+
--- src/views/User.vue
+++ src/views/User.vue
@@ -4,27 +4,26 @@
 <div class="flex m-10">
     <div class="flex flex-col w-2/5 p-5">
         
-        <user-card :name="name" :bio="bio" />
+        <user-card :name="user.name" :bio="user.bio" :avatar="user.avatar" />
 
         <div class="grid-flow-row shadow stats">
             <div class="stat">
                 <div class="stat-title">Posts</div> 
-                <div class="stat-value">10</div> 
+                <div class="stat-value">{{ user.posts-nb }}</div> 
             </div> 
             <div class="stat">
                 <div class="stat-title">Comments</div> 
-                <div class="stat-value">42</div> 
+                <div class="stat-value">{{ user.comments-nb }}</div> 
             </div> 
             <div class="stat">
                 <div class="stat-title">Likes</div> 
-                <div class="stat-value">1200</div> 
+                <div class="stat-value">{{ user.likes-nb }}</div> 
             </div>
         </div>
     </div>
 
     <div class="flex w-3/5 p-10">
         <h1 class="text-6xl">Flux</h1>
-  <div>Item: {{ $route.params.id }}</div>
 
     </div>
 </div>
@@ -37,9 +36,11 @@
 import Header from '../components/header.vue'
 import Footer from '../components/footer.vue'
 import UserCard from '../components/User/UserCard.vue'
+import GroupomaniaData from '../services/Data'
 </script>
 
 <script>
+
 export default {
     name: "MyProfil",
     components: {
@@ -47,9 +48,22 @@ export default {
     },
     data() {
         return {
-            name: "Michel",
-            bio: "Du service info et au service de l'humour"
+            user: []
         }
+    },
+    methods: {
+        searchUser() {
+            GroupomaniaData.getUser(this.$route.params.id)
+            .then((result) => {
+                this.user = result.data
+                console.log(result.data)
+            }).catch((err) => {
+                console.log(err)
+            });
+        }
+    },
+    mounted() {
+        this.searchUser()
     }
 }
 </script>
