5b2cec725bcc56bb2d08c1a9f03594322dcd0ad0
NaimePasLesPseudos <naimdelben@gmail.com>
2021-10-30 14:23:56 +0200

    Update database.js

--- backend/scripts/database.js
+++ backend/scripts/database.js
@@ -3,6 +3,8 @@
  */
 const Post = require('../app/models/Post')
 const User = require('../app/models/User')
+const Comment = require('../app/models/Comment')
+const bcrypt = require('bcrypt')
 
 async function createSchema() {
     console.log("========= Start database creation process...")
@@ -22,17 +24,30 @@ async function dummyData() {
     try {
         console.log("=== Create a dummy post")
         
+        const superPassword = "MotDePasse"
+        const hash = await bcrypt.hash(superPassword, 10) 
+
         const user = await User.query().insertGraph({
-            name: "Moi",
-            email: "moi@lui.com",
+            name: "l'équipe communication",
+            email: "communication@groupomania.com",
             role: "admin",
-            password: "SuperMDP",
+            password: hash,
+            registred: new Date().toUTCString(),
+            lastLogin: new Date().toUTCString(),
         })
         
         const post = await Post.query().insertGraph({
             author_id: 1,
-            title: "Mon premier Post",
-            content: "je kifferai tellement bosser sur Hajime ! <3",
+            title: "Bienvenue sur Groupomania.",
+            content: "L'installation semble s'être bien déroulée. Ceci est le premier article de votre nouveau réseau social d'entreprise - Groupomania ! Faites un tour des fonctionnalités que vous proposes ce site : Rajouter un article, commenter un article ou mettre une réaction. À bientôt !",
+            published: new Date().toUTCString(),
+            updated: new Date().toUTCString(),
+        })
+
+        const comment = await Comment.query().insertGraph({
+            post_id: 1,
+            author_id: 1,
+            content: "Ceci est votre premier commentaire sur le réseau social d'entreprise. Vous pouvez répondre à chacun des articles et votre commentaire s'affichera en dessous. Avec bienvaillance les réponses.",
             published: new Date().toUTCString(),
             updated: new Date().toUTCString(),
         })
