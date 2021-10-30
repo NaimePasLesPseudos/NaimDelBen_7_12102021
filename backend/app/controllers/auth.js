b532b2810603224d472643e3bacad0a94e54e95a
NaimePasLesPseudos <naimdelben@gmail.com>
2021-10-30 14:23:50 +0200

    hash with bcrypt

--- backend/app/controllers/auth.js
+++ backend/app/controllers/auth.js
@@ -1,12 +1,22 @@
 const User = require('../models/User')
+    , jwt = require('jsonwebtoken')
+    , bcrypt = require('bcrypt')
+    , dotenv = require('dotenv')
+
+dotenv.config()
 
 exports.signup = async (req, res, next) => {
     try {
+        const pwd = await bcrypt.hash(req.body.password, 10)
+
+
         const user = await User.query().insertGraph({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: pwd,
             role: "user",
+            picture: "https://placekitten.com/400/400",
+            background: "http://placeimg.com/600/600/arch",
             registred: new Date().toUTCString(),
             lastLogin: new Date().toUTCString()
         })
@@ -20,27 +30,36 @@ exports.signup = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
     try {
-        // const user = await User.query().where('email', req.body.email)
         const user = await User.query().where('email', req.body.email)
 
         if (user.length < 1) {
             return res.status(401).json({ error: "Mauvais e-mail !" })
         }
 
-// TODO: rajouter jwt et la vérif du token
-        const pwd = await User.query().where('password', req.body.password)
-
-        if (pwd.length < 1) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" })
-        }
-
-        const succes = await User
-            .query()
+        const pwd = await User.query()
             .where('email', req.body.email)
-            .patch({lastLogin: new Date().toUTCString()})
-            
-        res.json(user)
-        console.log(user)
+            .select('password')
+        
+        bcrypt.compare(req.body.password, pwd[0].password).then(match => {
+            if (!match) {
+                return res.status(401).json({ error: "Mot de passe incorrect !" })
+            }
+            User.query()
+                .where('email', req.body.email)
+                .patch({lastLogin: new Date().toUTCString()})
+                
+            res.status(200).json({
+                user: user[0].email,
+                token: jwt.sign(
+                    {userId: user[0].email },
+                    process.env.TOKEN_SECRET,
+                    { expiresIn: '24h' }
+                )
+        })
+        
+
+
+        })
     } catch (e) {
         return next(e)
     }
