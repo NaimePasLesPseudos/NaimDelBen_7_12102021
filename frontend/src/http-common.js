3a2d93566c74eb3fce1bb926b675c4243893efd2
NaimePasLesPseudos <naimdelben@gmail.com>
2021-11-07 17:43:32 +0100

    Delete http-common.js

--- frontend/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export default axios.create({
-    baseURL: "https://616961c209e030001712c33c.mockapi.io/",
-    headers: {
-        "Content-type": "application/json"
-    }
-})
