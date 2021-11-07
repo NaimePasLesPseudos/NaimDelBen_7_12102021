11d9528407a88d382ce0681a43fb417ccf3578b5
NaimePasLesPseudos <naimdelben@gmail.com>
2021-11-07 17:39:22 +0100

    fix header and add reactions' route

--- backend/app/index.js
+++ backend/app/index.js
@@ -4,13 +4,14 @@ const express = require('express')
     , authRoutes = require('./routes/auth')
     , postRoutes = require('./routes/posts')
     , commentRoutes = require('./routes/comments')
-    // , reactionRoutes = require('./app/routes/reaction')
+    , reactionRoutes = require('./routes/reactions')
     , auth = require('./middleware/auth')
     , path = require('path')
     , dotenv = require('dotenv')
     , app = express()
     , Knex = require('knex')
     , knexConfig = require('./config/knexfile')
+    , cors = require('cors')
 
 dotenv.config()
 
@@ -20,7 +21,7 @@ const knex = Knex(knexConfig.default)
 // Heroku connection test
 knex.raw("SELECT 1")
     .then(() => {
-        console.log("Heroku PostgreSQL connected !")
+        console.log("Hello World - Heroku PostgreSQL connected !")
     })
     .catch((e) => {
         console.log('Heroku PostgreSQL not connected !')
@@ -28,11 +29,12 @@ knex.raw("SELECT 1")
     })
 
 // Headers
+app.use(cors())
 app.use(helmet())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, X-auth-Token')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE, PATCH')
     next()
 })
 
@@ -46,7 +48,7 @@ app.use(auth.n)
 app.use('/v1/users', userRoutes)
 app.use('/v1/auth', authRoutes)
 app.use('/v1/posts', postRoutes)
-app.use('/v1/comments', commentRoutes)
-// app.use('', reactionRoutes)
+app.use('/v1/posts', commentRoutes)
+app.use('/v1/reactions', reactionRoutes)
 
 module.exports = app
