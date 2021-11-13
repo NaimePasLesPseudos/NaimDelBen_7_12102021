a8696b3f9f9fc93ca0e3e9ec673dedf79229bc49
NaimePasLesPseudos <naimdelben@gmail.com>
2021-11-13 09:37:57 +0100

    add Role in Token

--- backend/app/middleware/auth.js
+++ backend/app/middleware/auth.js
@@ -3,29 +3,33 @@ const jwt = require('jsonwebtoken')
 
 dotenv.config()
 
-// Authentification de l'utilisateur
+// If authorization header, retrieve auth user
 exports.n = async (req, res, next) => {
-    req.userId = null;
+    req.userId = null
+    req.role = null
+    
     if (req.headers.authorization) {
         // Le client a envoyé un header avec un token ?
         const token = req.headers.authorization.split(' ')[1]
         try {
             // On vérifie le token et on récupère son payload
-            const { userId } = jwt.verify(token, process.env.TOKEN_SECRET);
-            req.userId = userId;
+            const { userId, role } = jwt.verify(token, process.env.TOKEN_SECRET)
+            req.userId = userId
+            req.role = role
             console.log('authn : succes !')
         } catch (err) {
             console.log('authn : failed !')
         }
     }
-    next();
+
+    next()
 }
 
-// Authorisation de l'utilisateur
+// Check that user is authentified
 exports.z = async (req, res, next) => {
     if (!req.userId) {
         return res.status(401).json({ error: '401: User is not authenticated' })
     }
     console.log('authz : succes !')
-    next();
+    next()
 }
