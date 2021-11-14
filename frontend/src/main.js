179e08880420d8b32f9916b496c6d6e68595df29
NaimePasLesPseudos <naimdelben@gmail.com>
2021-11-14 21:58:06 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/index.css'
-import router from '@/router/index'
+import store from './store'
+import { createRouterWithStore } from '@/router/index'
+import Toast, { POSITION } from 'vue-toastification'
+import "vue-toastification/dist/index.css";
+
+const router = createRouterWithStore(store)
+
+const toastOptions = {
+    position: POSITION.BOTTOM_CENTER
+}
 
 createApp(App)
+    .use(store)
+    .use(Toast, toastOptions)
     .use(router)
     .mount('#app')
